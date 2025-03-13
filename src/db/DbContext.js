b9dfdf0225060ad737fa2deafcbae6f20ccc6775
d-hooper <dallinhoop3@gmail.com>
2025-03-13 14:30:53 -0600

    init, env working, created Bug schema

---
+++ src/db/DbContext.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import { AccountSchema } from '../models/Account.js'
+import { BugSchema } from '../models/Bug.js';
+
+class DbContext {
+  Account = mongoose.model('Account', AccountSchema);
+
+  Bug = mongoose.model('Bug', BugSchema);
+
+}
+
+export const dbContext = new DbContext()
---
+++ www/env.js
@@ -0,0 +1,3 @@
+export const domain = 'dev-5x00quh2000bn67r.us.auth0.com'
+export const clientId = 'rIgFRlgvMf2EjU8FMoLrmIe6jiycAVT5'
+export const audience = 'https://auth.speedwagonfoundation.com'
